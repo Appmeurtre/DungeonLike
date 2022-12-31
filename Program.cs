71bf61af11b43ab637a4a2bbec07658da78d6d82
Appmeurtre <appmeurtre@gmail.com>
2022-12-31 17:08:34 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,104 +1,233 @@
 using System;
-using System.Collections.Generic;
 
-namespace DungeonLike
+
+namespace RougeLike
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Player player = new Player();
+            int CharacterHealth_Current = 100;
+            int CharacterHealth_Max = 100;
+            int Score = 0;
+            int GameOver = 0;
+            int RoomCounter = 1;
+            int RoomsCounted = 0;
+            int RoadSteps = 1;
+            int CharacterMovementUpDown = 0;
+            int CharacterMovementLeftRight = 0;
 
-            player.X = 1;
-            player.Y = 1;
+            Random Random = new Random();
 
-            List<Enemy> enemies = new List<Enemy>();
+            string DrawCharacter = "D";
+            string[,] DrawMap = new string[119, 29];
+            string Direction = "";
 
-            enemies.Add(new Enemy { X = 5, Y = 5 });
-            enemies.Add(new Enemy { X = 10, Y = 10 });
-
-            while (true)
+            do
             {
-
                 Console.Clear();
+                Console.CursorVisible = false;
+                Console.SetCursorPosition(0, 29);
+                Console.Write("Health: " + CharacterHealth_Current + "/" + CharacterHealth_Max + " | Score: " + Score);
 
-                DrawMap(player, enemies);
-
-                ConsoleKeyInfo keyInfo = Console.ReadKey(true);
-
-                switch (keyInfo.Key)
+                if (RoomsCounted != RoomCounter)
                 {
-                    case ConsoleKey.UpArrow:
-                        player.Y--;
-                        break;
-                    case ConsoleKey.DownArrow:
-                        player.Y++;
-                        break;
-                    case ConsoleKey.LeftArrow:
-                        player.X--;
-                        break;
-                    case ConsoleKey.RightArrow:
-                        player.X++;
-                        break;
-                }
+                    int SpawnPointHeight = Random.Next(5, 8);
+                    int SpawnPointWidth = Random.Next(7, 10);
+                    int SpawnPointX = Random.Next(0, 119 - SpawnPointWidth - 1);
+                    int SpawnPointY = Random.Next(0, 29 - SpawnPointHeight - 1);
 
-                foreach (Enemy enemy in enemies)
-                {
-                    if (player.X == enemy.X && player.Y == enemy.Y)
-                    {
-                        Console.Clear();
-                        Console.WriteLine("You have been defeated by the enemy!");
-                        return;
-                    }
-                }
-            }
-        }
+                    CharacterMovementLeftRight = SpawnPointX + 1;
+                    CharacterMovementUpDown = SpawnPointY + 1;
 
-        static void DrawMap(Player player, List<Enemy> enemies)
-        {
-            for (int y = 0; y < 20; y++)
-            {
-                for (int x = 0; x < 20; x++)
-                {
-                    if (player.X == x && player.Y == y)
+                    for (int y = 0; y <= SpawnPointHeight; y++)
                     {
-                        Console.Write("P");
-                    }
-                    else
-                    {
-                        bool enemyDrawn = false;
-
-                        foreach (Enemy enemy in enemies)
+                        for (int x = 0; x <= SpawnPointWidth; x++)
                         {
-                            if (enemy.X == x && enemy.Y == y)
+                            if (SpawnPointX + SpawnPointWidth >= 119)
                             {
-                                Console.Write("E");
-                                enemyDrawn = true;
-                                break;
+                                int X_Difference = SpawnPointX + SpawnPointWidth - 120;
+                                SpawnPointX = SpawnPointX - X_Difference;
+                            }
+
+                            if (SpawnPointY + SpawnPointHeight >= 28)
+                            {
+                                int Y_Difference = SpawnPointY + SpawnPointHeight - 29;
+                                SpawnPointY = SpawnPointY - Y_Difference;
+                            }
+
+                            if (y == 0 || y == SpawnPointHeight)
+                            {
+                                DrawMap[SpawnPointX + x, SpawnPointY + y] = "_";
+                            }
+                            else
+                            {
+                                if (x == 0 || x == SpawnPointWidth)
+                                {
+                                    DrawMap[SpawnPointX + x, SpawnPointY + y] = "|";
+                                }
+                                else
+                                {
+                                    DrawMap[SpawnPointX + x, SpawnPointY + y] = ".";
+                                }
                             }
                         }
+                    }
+                    
+                    
+                    int SpawnFirstRoadRoll = Random.Next(1, 3);
+                    if (SpawnFirstRoadRoll == 1)
+                    {
+                        int SpawnFirstRoadY = Random.Next(1, SpawnPointHeight - 1);
 
-                        if (!enemyDrawn)
+                        if (SpawnPointX <= 59)
                         {
-                            Console.Write(".");
+                            Direction = "Right";
+                            DrawMap[SpawnPointX + SpawnPointWidth, SpawnPointY + SpawnFirstRoadY + 1] = "#";
+                        }
+
+                        if (SpawnPointX > 59)
+                        {
+                            Direction = "Left";
+                            DrawMap[SpawnPointX, SpawnPointY + SpawnFirstRoadY + 1] = "#";
+                        }
+                    }
+
+                    if (SpawnFirstRoadRoll == 2)
+                    {
+                        int SpawnFirstRoadY = Random.Next(1, SpawnPointWidth - 1);
+                        if (SpawnPointY <= 15)
+                        {
+                            Direction = "Down";
+                            DrawMap[SpawnPointX + SpawnFirstRoadY + 1, SpawnPointY] = "#";
+                        }
+
+                        if (SpawnPointY > 15)
+                        {
+                            Direction = "Up";
+                            DrawMap[SpawnPointX + SpawnFirstRoadY + 1, SpawnPointY] = "#";
+                        }
+                    }
+
+                    RoomsCounted++;
+                }
+
+                for (int yDraw = 0; yDraw <= 28; yDraw++)
+                {
+                    for (int xDraw = 0; xDraw <= 118; xDraw++)
+                    {
+                        Console.SetCursorPosition(xDraw, yDraw);
+                        Console.Write(DrawMap[xDraw, yDraw]);
+                    }
+                }
+
+                ConsoleKeyInfo keyInfo;
+                keyInfo = Console.ReadKey(true);
+                switch (keyInfo.Key)
+                {
+                    case ConsoleKey.RightArrow:
+                        if (CharacterMovementLeftRight < 118)
+                        {
+                            if (DrawMap[CharacterMovementLeftRight + 1, CharacterMovementUpDown] == "|" || DrawMap[CharacterMovementLeftRight, CharacterMovementUpDown + 1] == null)
+                            {
+                            }
+                            else
+                            {
+                                CharacterMovementLeftRight++;
+                                DrawMap[CharacterMovementLeftRight, CharacterMovementUpDown] = "X";
+                                DrawMap[CharacterMovementLeftRight - 1, CharacterMovementUpDown] = ".";
+                            }
+                        }
+                        else { }
+                        break;
+
+                    case ConsoleKey.LeftArrow:
+                        if(CharacterMovementLeftRight > 1)
+                        {
+                            if (DrawMap[CharacterMovementLeftRight -1, CharacterMovementUpDown] == "|" || DrawMap[CharacterMovementLeftRight, CharacterMovementUpDown +1] == null)
+                            {
+
+                            }
+                            else
+                            {
+                                CharacterMovementLeftRight--;
+                                DrawMap[CharacterMovementLeftRight, CharacterMovementUpDown] = "X";
+                                DrawMap[CharacterMovementLeftRight + 1, CharacterMovementUpDown] = ".";
+                            }
+                        }
+                        else
+                        {
+
+                        }
+                        break;
+                    case ConsoleKey.UpArrow:
+                        if (CharacterMovementUpDown > 0)
+                        {
+                            if (DrawMap[CharacterMovementLeftRight, CharacterMovementUpDown - 1] == "_" || DrawMap[CharacterMovementLeftRight, CharacterMovementUpDown + 1] == null)
+                            {
+
+                            }
+                            else
+                            {
+                                CharacterMovementUpDown--;
+                                DrawMap[CharacterMovementLeftRight, CharacterMovementUpDown] = "X";
+                                DrawMap[CharacterMovementLeftRight, CharacterMovementUpDown + 1] = ".";
+                            }
+                        }
+                        else
+                        {
+
+                        }
+                        break;
+                    case ConsoleKey.DownArrow:
+                        if (CharacterMovementUpDown < 27)
+                        {
+                            if (DrawMap[CharacterMovementLeftRight, CharacterMovementUpDown + 1] == "_" || DrawMap[CharacterMovementLeftRight, CharacterMovementUpDown + 1] == null)
+                            {
+
+                            }
+                            else
+                            {
+                                CharacterMovementUpDown++;
+                                DrawMap[CharacterMovementLeftRight, CharacterMovementUpDown] = "X";
+                                DrawMap[CharacterMovementLeftRight, CharacterMovementUpDown - 1] = ".";
+                            }
+                        }
+                        else
+                        {
+
+                        }
+                        break;
+                }
+
+                if(DrawMap[CharacterMovementLeftRight, CharacterMovementUpDown] == "#")
+                {
+                    Console.SetCursorPosition(100, 29);
+                    Console.Write("True");
+                    RoadSteps++;
+
+                    if (RoadSteps >= 1)
+                    {
+                        if (Direction == "Right")
+                        {
+                            DrawMap[CharacterMovementLeftRight + 1, CharacterMovementUpDown] = "#";
+                        }
+                        if (Direction == "Left")
+                        {
+                            DrawMap[CharacterMovementLeftRight - 1, CharacterMovementUpDown] = "#";
+                        }
+                        if (Direction == "Up")
+                        {
+                            DrawMap[CharacterMovementLeftRight, CharacterMovementUpDown - 1] = "#";
+                        }
+                        if (Direction == "Down")
+                        {
+                            DrawMap[CharacterMovementLeftRight, CharacterMovementUpDown + 1] = "#";
                         }
                     }
                 }
 
-                Console.WriteLine();
-            }
+            } while (GameOver == 0);
         }
-    }
-
-    class Player
-    {
-        public int X { get; set; }
-        public int Y { get; set; }
-    }
-
-    class Enemy
-    {
-        public int X { get; set; }
-        public int Y { get; set; }
     }
 }
